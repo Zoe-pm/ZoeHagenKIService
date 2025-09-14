00b44d271887be5cdf40441c7c150ff63b5e0bb0
zbhagen <39544687-zbhagen@users.noreply.replit.com>
2025-09-14 16:31:40 +0000

    Restored to 'f886420e6ebdea03f81ea945bec491be0a39a797'
    Replit-Restored-To: f886420e6ebdea03f81ea945bec491be0a39a797

--- client/src/pages/UeberUns.tsx
+++ client/src/pages/UeberUns.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import SEOHelmet from "@/components/SEOHelmet";
 import Navigation from "@/components/Navigation";
 import ChatbotWidget from "@/components/ChatbotWidget";
-const zoeImage = "/images/zoe-portrait.jpg";
+const zoeImage = "/images/zoe-image.jpg";
 
 export default function UeberUns() {
   // Automatisch zum Seitenbeginn scrollen
